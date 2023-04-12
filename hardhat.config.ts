fc2d069e3269137e328853220a2f1c8d1f6661fd
schultztimothy <schultz.timothy52@gmail.com>
2023-04-12 15:26:26 -0600

    use env variable for rpc

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,12 +3,14 @@ import "@nomicfoundation/hardhat-toolbox";
 import "@typechain/hardhat";
 import "@nomiclabs/hardhat-ethers";
 
+const sepoliaRpc = `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`;
+
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
   networks: {
     hardhat: {
       forking: {
-        url: "https://eth-sepolia.g.alchemy.com/v2/3RODDJGW-ZoZwHYkgKHXjwMYlURKMHDv",
+        url: sepoliaRpc,
       },
     },
   },
