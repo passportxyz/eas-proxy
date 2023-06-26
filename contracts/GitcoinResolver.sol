489a79846de91afd38aee1ddce7ffdf79875076f
Gerald Iakobinyi-Pich <nutrina9@gmail.com>
2023-06-26 11:50:54 +0300

    feat(eas): specify correct inheritance for resolver sc

--- contracts/GitcoinResolver.sol
+++ contracts/GitcoinResolver.sol
@@ -1,11 +1,11 @@
-
 // SPDX-License-Identifier: GPL
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-import { AttestationRequest, AttestationRequestData, EAS, Attestation, MultiAttestationRequest } from "@ethereum-attestation-service/eas-contracts/contracts/EAS.sol";
+import {AttestationRequest, AttestationRequestData, EAS, Attestation, MultiAttestationRequest} from "@ethereum-attestation-service/eas-contracts/contracts/EAS.sol";
+import {ISchemaResolver} from "@ethereum-attestation-service/eas-contracts/contracts/resolver/ISchemaResolver.sol";
 
 import "./GitcoinAttester.sol";
 
@@ -14,6 +14,5 @@ import "./GitcoinAttester.sol";
  * @notice This contract is used to verify a passport's authenticity and to add a passport to the GitcoinAttester contract using the verifyAndAttest() function.
  */
 contract GitcoinResolver {
-  
 
 }
