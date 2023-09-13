dd3c628247d878cb778c71a0522b2685e1f3a287
schultztimothy <schultz.timothy52@gmail.com>
2023-09-13 17:11:53 -0600

    feat(contracts): seemingly correct hashing functions

--- contracts/GitcoinStampVerifier.sol
+++ contracts/GitcoinStampVerifier.sol
@@ -1,74 +1,132 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.8.4;
+// SPDX-License-Identifier: GPL
+pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import { VcVerifier } from "./VCVerifier.sol";
-import { DIDpkhAdapter } from "./DIDpkhAdapter.sol";
-import { AttestationStation } from "./AttestationStation.sol";
 
-struct CredentialSubject {
-    // underscored since hash is a reserved keyword
-    string _hash;
-    string id;
-    string provider;
-}
+import "hardhat/console.sol";
 
-struct Proof {
-    // underscored since @ is not valid for struct member
-    string _context;
-    string created;
-    string proofPurpose;
-    // underscored since typoe is a reserved keyword
-    string _type;
-    string verificationMethod;
-}
+import "./GitcoinAttester.sol";
 
-struct Document {
-    // underscored since @ is not valid for struct member
-    string _context;
-    CredentialSubject credentialSubject;
-    string expirationDate;
-    string issuanceDate;
-    string issuer;
-    Proof proof;
-    // underscored since @ is not valid for struct member
-    string[] _type;
-}
+/**
+ * @title GitcoinVerifier
+ * @notice This contract is used to verify a passport's authenticity and to add a passport to the GitcoinAttester contract using the verifyAndAttest() function.
+ */
+contract GitcoinStampVerifier {
+    using ECDSA for bytes32;
 
-contract DIDStampVcVerifier is VcVerifier, DIDpkhAdapter {
+    // Address of the issuer of the passport
+    address public issuer;
+
+    // Domain Separator, as defined by EIP-712 (`hashstruct(eip712Domain)`)
+    bytes32 private DOMAIN_SEPARATOR;
+
+    // Name of the contract
+    string public name;
+
+    struct Proof {
+        // underscored since @ is not valid for struct member
+        string _context;
+                // underscored since typoe is a reserved keyword
+        string _type;
+        string proofPurpose;
+        string verificationMethod;
+        string created;
+    }
+
+    struct CredentialSubjectContext {
+        string customInfo;
+        string _hash;
+        string metaPointer;
+        string provider;
+    }
+
+    struct Customifo {
+        string description;
+    }
+
+    struct CredentialSubject {
+        string id;
+        CredentialSubjectContext _context;
+        string provider;
+        // Customifo customInfo;
+        // underscored since hash is a reserved keyword
+        string _hash;
+    }
+
+
+    struct Document {
+        // underscored since @ is not valid for struct member
+        string[] _context;
+        // underscored since @ is not valid for struct member
+        string[] _type;
+        CredentialSubject credentialSubject;
+        string issuer;
+        string issuanceDate;
+        Proof proof;
+        string expirationDate;
+    }
+
     bytes32 private constant PROOF_TYPE_HASH =
-        keccak256("Proof(string @context,string created,string proofPurpose,string type,string verificationMethod)");
+        keccak256("Proof(string @context,string type,string proofPurpose,string verificationMethod,string created)");
+
+    bytes32 private constant CREDENTIAL_SUBJECT_CONTEXT_TYPEHASH =
+        keccak256("CredentialSubjectContext(string customInfo,string hash,string metaPointer,string provider)");
+
+    // bytes32 private constant CUSTOMINFO_TYPEHASH = keccak256("CustomInfo(string description)");
 
     bytes32 private constant CREDENTIAL_SUBJECT_TYPEHASH =
-        keccak256("CredentialSubject(string hash,string id,string provider)");
+        keccak256(
+            "CredentialSubject(string id,CredentialSubjectContext @context,string provider,string hash)"
+        );
 
     bytes32 private constant DOCUMENT_TYPEHASH =
         keccak256(
-            "Document(string @context,CredentialSubject credentialSubject,string expirationDate,string issuanceDate,string issuer,Proof proof,string[] type)CredentialSubject(string hash,string id,string provider)Proof(string @context,string created,string proofPurpose,string type,string verificationMethod)"
+            "Document(string[] @context,string[] type,CredentialSubject credentialSubject,string issuer,string issuanceDate,Proof proof,string expirationDate)"
         );
 
-    address public _verifier;
-    address public _attestationStation;
+    bytes32 private constant EIP712DOMAIN_TYPEHASH = keccak256("EIP712Domain(string name,string version)");
 
-    AttestationStation.AttestationData[] public _attestations;
+    /**
+    * @notice Initializer function responsible for setting up the contract's initial state.
+    * @param _issuer The address of the issuer of the passport.
+    */
+    function initialize(address _issuer) public {
 
-    event Verified(string indexed id, string iamHash, string provider);
+        issuer = _issuer;
+        name = "Gitcoin Passport Verifiable Credential of Stamp data";
 
-    mapping(string => string) public verifiedStamps;
-
-    constructor(string memory domainName, address verifier, address attestationStation) VcVerifier(domainName) {
-        _verifier = verifier;
-        _attestationStation = attestationStation;
+        DOMAIN_SEPARATOR = keccak256(
+            abi.encode(
+                EIP712DOMAIN_TYPEHASH,
+                keccak256(bytes(name)),
+                keccak256(bytes("0"))
+            )
+        );
     }
 
+    function hashCredentialSubjectContext(CredentialSubjectContext calldata context) public pure returns (bytes32) {
+        return keccak256(
+            abi.encode(
+                CREDENTIAL_SUBJECT_CONTEXT_TYPEHASH,
+                keccak256(bytes(context.customInfo)),
+                keccak256(bytes(context._hash)),
+                keccak256(bytes(context.metaPointer)),
+                keccak256(bytes(context.provider))
+            )
+        );
+    }
+
+
     function hashCredentialSubject(CredentialSubject calldata subject) public pure returns (bytes32) {
+        bytes32 credentialSubjectContext = hashCredentialSubjectContext(subject._context);
         return
             keccak256(
                 abi.encode(
                     CREDENTIAL_SUBJECT_TYPEHASH,
-                    keccak256(bytes(subject._hash)),
                     keccak256(bytes(subject.id)),
-                    keccak256(bytes(subject.provider))
+                    credentialSubjectContext,
+                    keccak256(bytes(subject.provider)),
+                    keccak256(bytes(subject._hash))
                 )
             );
     }
@@ -79,62 +137,54 @@ contract DIDStampVcVerifier is VcVerifier, DIDpkhAdapter {
                 abi.encode(
                     PROOF_TYPE_HASH,
                     keccak256(bytes(proof._context)),
-                    keccak256(bytes(proof.created)),
-                    keccak256(bytes(proof.proofPurpose)),
                     keccak256(bytes(proof._type)),
-                    keccak256(bytes(proof.verificationMethod))
+                    keccak256(bytes(proof.proofPurpose)),
+                    keccak256(bytes(proof.verificationMethod)),
+                    keccak256(bytes(proof.created))
                 )
             );
+    }
+
+    function _hashArray(string[] calldata array) internal pure returns (bytes32 result) {
+        bytes32[] memory _array = new bytes32[](array.length);
+        for (uint256 i = 0; i < array.length; ++i) {
+            _array[i] = keccak256(bytes(array[i]));
+        }
+        result = keccak256(abi.encodePacked(_array));
     }
 
     function hashDocument(Document calldata document) public pure returns (bytes32) {
         bytes32 credentialSubjectHash = hashCredentialSubject(document.credentialSubject);
         bytes32 proofHash = hashCredentialProof(document.proof);
 
+        console.log(document._context, "document._context");
         return
             keccak256(
                 abi.encode(
                     DOCUMENT_TYPEHASH,
-                    keccak256(bytes(document._context)),
+                    _hashArray(document._context),
+                    _hashArray(document._type),
                     credentialSubjectHash,
-                    keccak256(bytes(document.expirationDate)),
-                    keccak256(bytes(document.issuanceDate)),
                     keccak256(bytes(document.issuer)),
+                    keccak256(bytes(document.issuanceDate)),
                     proofHash,
-                    _hashArray(document._type)
+                    keccak256(bytes(document.expirationDate))
                 )
             );
     }
 
-    function verifyStampVc(Document calldata document, uint8 v, bytes32 r, bytes32 s) public returns (bool) {
+    function verifyStampVc(Document calldata document, uint8 v, bytes32 r, bytes32 s) public view returns (bool) {
         bytes32 vcHash = hashDocument(document);
         bytes32 digest = ECDSA.toTypedDataHash(DOMAIN_SEPARATOR, vcHash);
 
-        address issuerAddress = DIDpkhAdapter.pseudoResolveDidIssuer(document.issuer);
-
         address recoveredAddress = ECDSA.recover(digest, v, r, s);
+
+        console.log(recoveredAddress, issuer, "recoveredAddress, issuer, ");
 
         // Here we could check the issuer's address against an on-chain registry.
         // We could provide a verifying contract address when signing the credential which could correspond to this contract
-        require(recoveredAddress == issuerAddress, "VC verification failed issuer does not match signature");
+        require(recoveredAddress == issuer, "VC verification failed issuer does not match signature");
 
-        verifiedStamps[document.credentialSubject.id] = document.credentialSubject._hash;
-
-        emit Verified(
-            document.credentialSubject.id,
-            document.credentialSubject._hash,
-            document.credentialSubject.provider
-        );
-
-        AttestationStation attestationStation = AttestationStation(_attestationStation);
-        AttestationStation.AttestationData memory attestation = AttestationStation.AttestationData(
-            msg.sender,
-            "Verified",
-            "yes"
-        );
-        _attestations.push(attestation);
-
-        attestationStation.attest(_attestations);
         return true;
     }
 }
--- test/GitcoinStampVerifier.ts
+++ test/GitcoinStampVerifier.ts
@@ -2,29 +2,44 @@ import { Signature } from "ethers";
 import { ethers } from "hardhat";
 import { PromiseOrValue } from "../typechain-types/common";
 import { expect } from "chai";
+import { GitcoinStampVerifier__factory } from "../typechain-types";
 
 export type ProofStruct = {
   _context: PromiseOrValue<string>;
-  created: PromiseOrValue<string>;
-  proofPurpose: PromiseOrValue<string>;
   _type: PromiseOrValue<string>;
+  proofPurpose: PromiseOrValue<string>;
+  // proofValue?: PromiseOrValue<string>;
   verificationMethod: PromiseOrValue<string>;
+  created: PromiseOrValue<string>;
 };
 
+export type CredentialSubjectContext = {
+  customInfo: string;
+  _hash: string;
+  metaPointer: string;
+  provider: string;
+};
+
+// export type CustomInfoStruct = {
+//   description: string;
+// };
+
 export type CredentialSubjectStruct = {
-  _hash: PromiseOrValue<string>;
   id: PromiseOrValue<string>;
+  _context: CredentialSubjectContext;
   provider: PromiseOrValue<string>;
+  // customInfo: CustomInfoStruct;
+  _hash: PromiseOrValue<string>;
 };
 
 export type DocumentStruct = {
   _context: string[];
-  credentialSubject: CredentialSubjectStruct;
-  expirationDate: PromiseOrValue<string>;
-  issuanceDate: PromiseOrValue<string>;
-  issuer: PromiseOrValue<string>;
-  proof: ProofStruct;
   _type: PromiseOrValue<string>[];
+  credentialSubject: CredentialSubjectStruct;
+  issuer: PromiseOrValue<string>;
+  issuanceDate: PromiseOrValue<string>;
+  proof: ProofStruct;
+  expirationDate: PromiseOrValue<string>;
 };
 
 export interface DIDCredential {
@@ -40,9 +55,9 @@ export interface CredentialSubject {
   id: string;
   provider: string;
   hash: string;
-  customInfo: {
-    [key: string]: string;
-  };
+  // customInfo: {
+  //   description: string;
+  // };
   "@context": {
     customInfo: string;
     hash: string;
@@ -89,10 +104,27 @@ export const normalizeDIDCredential = (credential: DIDCredential) => {
   const normalizedCredential = {} as DocumentStruct;
   const normalizedSubject = {} as CredentialSubjectStruct;
   const normalizedProof = {} as ProofStruct;
+  const normalizedCredentialContext = {} as {
+    customInfo: string;
+    _hash: string;
+    metaPointer: string;
+    provider: string;
+  };
 
   normalizedSubject["id"] = credential.credentialSubject.id;
   normalizedSubject["provider"] = credential.credentialSubject.provider;
   normalizedSubject["_hash"] = credential.credentialSubject.hash;
+
+  normalizedCredentialContext["customInfo"] =
+    credential.credentialSubject["@context"]["customInfo"];
+  normalizedCredentialContext["_hash"] =
+    credential.credentialSubject["@context"]["hash"];
+  normalizedCredentialContext["metaPointer"] =
+    credential.credentialSubject["@context"]["metaPointer"];
+  normalizedCredentialContext["provider"] =
+    credential.credentialSubject["@context"]["provider"];
+
+  normalizedSubject["_context"] = normalizedCredentialContext;
 
   normalizedProof["_context"] = credential.proof["@context"];
   normalizedProof["created"] = credential.proof.created;
@@ -115,36 +147,176 @@ export const normalizeDIDCredential = (credential: DIDCredential) => {
 };
 
 const sampleCredential = {
-  "@context": [
-    "https://www.w3.org/2018/credentials/v1",
-    "https://w3id.org/vc/status-list/2021/v1",
-  ],
   type: ["VerifiableCredential"],
-  credentialSubject: {
-    id: "did:pkh:eip155:1:0xC79ABB54e4824Cdb65C71f2eeb2D7f2db5dA1fB8",
-    "@context": {
-      customInfo: "https://schema.org/Thing",
-      hash: "https://schema.org/Text",
-      metaPointer: "https://schema.org/URL",
-      provider: "https://schema.org/Text",
-    },
-    provider: "githubAccountCreationGte#90",
-    customInfo: {},
-    hash: "v0.0.0:KkYaKn2GaF55a3y/n6myG9kfrpQPHW5DnhhzO9APTGI=",
-  },
-  issuer: "did:ethr:0xd6fc34345bc8c8e5659a35bed9629d5558d48c4e",
-  issuanceDate: "2023-09-13T02:12:37.417Z",
   proof: {
     type: "EthereumEip712Signature2021",
-    created: "2023-09-13T02:12:37.422Z",
+    created: "2023-09-13T21:25:13.163Z",
     "@context": "https://w3id.org/security/suites/eip712sig-2021/v1",
     proofValue:
-      "0x17d27bfcd590ce7bd26b1c6ebd953d9f390f2089279403733acf874366f768697dab73a4fdb1ea380a0feea129b521245d1b2ffcd69b4e2586cda64b3c7c770a1c",
+      "0x1139ee60eab7323a7ec73427ece720642e93b1e29ab854c6942fc04d1b08aeaa16cd6e158f721eec74d115fb8e3ea29b0dd1f4b37167325e67a905bd5abc72431b",
+    eip712Domain: {
+      types: {
+        Proof: [
+          {
+            name: "@context",
+            type: "string",
+          },
+          {
+            name: "type",
+            type: "string",
+          },
+          {
+            name: "proofPurpose",
+            type: "string",
+          },
+          {
+            name: "proofValue",
+            type: "string",
+          },
+          {
+            name: "verificationMethod",
+            type: "string",
+          },
+          {
+            name: "created",
+            type: "string",
+          },
+        ],
+        Context: [
+          {
+            name: "customInfo",
+            type: "string",
+          },
+          {
+            name: "hash",
+            type: "string",
+          },
+          {
+            name: "metaPointer",
+            type: "string",
+          },
+          {
+            name: "provider",
+            type: "string",
+          },
+        ],
+        Document: [
+          {
+            name: "@context",
+            type: "string[]",
+          },
+          {
+            name: "type",
+            type: "string[]",
+          },
+          {
+            name: "issuer",
+            type: "string",
+          },
+          {
+            name: "issuanceDate",
+            type: "string",
+          },
+          {
+            name: "expirationDate",
+            type: "string",
+          },
+          {
+            name: "credentialSubject",
+            type: "CredentialSubject",
+          },
+          {
+            name: "proof",
+            type: "Proof",
+          },
+          {
+            name: "credentialStatus",
+            type: "CredentialStatus",
+          },
+        ],
+        EIP712Domain: [
+          {
+            name: "name",
+            type: "string",
+          },
+          {
+            name: "version",
+            type: "string",
+          },
+        ],
+        CredentialStatus: [
+          {
+            name: "id",
+            type: "string",
+          },
+          {
+            name: "type",
+            type: "string",
+          },
+          {
+            name: "statusPurpose",
+            type: "string",
+          },
+          {
+            name: "statusListIndex",
+            type: "string",
+          },
+          {
+            name: "statusListCredential",
+            type: "string",
+          },
+        ],
+        CredentialSubject: [
+          {
+            name: "id",
+            type: "string",
+          },
+          {
+            name: "provider",
+            type: "string",
+          },
+          {
+            name: "metaPointer",
+            type: "string",
+          },
+          {
+            name: "hash",
+            type: "string",
+          },
+          {
+            name: "@context",
+            type: "Context",
+          },
+        ],
+      },
+      domain: {
+        name: "Gitcoin Passport Verifiable Credential of Stamp data",
+        version: "0",
+      },
+      primaryType: "Document",
+    },
     proofPurpose: "assertionMethod",
     verificationMethod:
       "did:ethr:0xd6fc34345bc8c8e5659a35bed9629d5558d48c4e#controller",
   },
-  expirationDate: "2023-12-12T03:12:37.417Z",
+  issuer: "did:ethr:0xd6fc34345bc8c8e5659a35bed9629d5558d48c4e",
+  "@context": [
+    "https://www.w3.org/2018/credentials/v1",
+    "https://w3id.org/vc/status-list/2021/v1",
+  ],
+  issuanceDate: "2023-09-13T21:25:13.150Z",
+  expirationDate: "2023-12-12T22:25:13.150Z",
+  credentialSubject: {
+    id: "did:pkh:eip155:1:0xC79ABB54e4824Cdb65C71f2eeb2D7f2db5dA1fB8",
+    hash: "v0.0.0:0xRiw2BMLTItJg8iicfNTLlzQumcF37sKIifVthEx9Y=",
+    "@context": {
+      hash: "https://schema.org/Text",
+      provider: "https://schema.org/Text",
+      customInfo: "https://schema.org/Thing",
+      metaPointer: "https://schema.org/URL",
+    },
+    provider: "EthGTEOneTxnProvider",
+  },
 };
 
 describe.only("Signature for GitcoinStampVerifier", () => {
@@ -152,20 +324,48 @@ describe.only("Signature for GitcoinStampVerifier", () => {
     const signature = Signature.from(sampleCredential.proof.proofValue);
     const { r, s, v } = signature;
     expect(r).to.be.equal(
-      "0x17d27bfcd590ce7bd26b1c6ebd953d9f390f2089279403733acf874366f76869"
+      "0x1139ee60eab7323a7ec73427ece720642e93b1e29ab854c6942fc04d1b08aeaa"
     );
     expect(s).to.be.equal(
-      "0x7dab73a4fdb1ea380a0feea129b521245d1b2ffcd69b4e2586cda64b3c7c770a"
+      "0x16cd6e158f721eec74d115fb8e3ea29b0dd1f4b37167325e67a905bd5abc7243"
     );
 
-    expect(v).to.be.equal(28);
+    expect(v).to.be.equal(27);
   });
   it("should normalize the credential for writing on chain", () => {
     const normalizedCredential = normalizeDIDCredential(sampleCredential);
-    debugger;
+
     expect(normalizedCredential._context).to.be.deep.equal([
       "https://www.w3.org/2018/credentials/v1",
       "https://w3id.org/vc/status-list/2021/v1",
     ]);
+  });
+  it("should verify the VC signature on chain", async () => {
+    const normalizedCredential = normalizeDIDCredential(
+      sampleCredential
+    ) as DocumentStruct;
+    const [signer] = await ethers.getSigners();
+
+    const GitcoinStampVerifier = await ethers.getContractFactory(
+      "GitcoinStampVerifier"
+    );
+
+    const gitcoinStampVerifier = await GitcoinStampVerifier.deploy();
+
+    await gitcoinStampVerifier.initialize(
+      "0xd6fc34345bc8c8e5659a35bed9629d5558d48c4e"
+    );
+    const signature = Signature.from(sampleCredential.proof.proofValue);
+    const { r, s, v } = signature;
+
+    debugger;
+    const tx = await gitcoinStampVerifier.verifyStampVc(
+      normalizedCredential,
+      v,
+      r,
+      s
+    );
+
+    expect(tx).to.be.true;
   });
 });
