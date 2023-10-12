12976f2085823d92f3e438a17b39de782a58085a
Aminah Burch <hello@aminah.io>
2023-10-12 23:42:28 +0300

    feat(contracts, test): fixes getPassport function and adds initial tests for it

--- contracts/GitcoinPassportDecoder.sol
+++ contracts/GitcoinPassportDecoder.sol
@@ -4,8 +4,11 @@ pragma solidity ^0.8.9;
 import { Initializable, OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import { PausableUpgradeable } from "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import { UUPSUpgradeable } from "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
+import { Attestation, IEAS } from "@ethereum-attestation-service/eas-contracts/contracts/EAS.sol";
 
 import { GitcoinResolver } from "./GitcoinResolver.sol";
+
+import "hardhat/console.sol";
 
 /**
  * @title GitcoinPassportDecoder
@@ -18,11 +21,27 @@ contract GitcoinPassportDecoder is
   OwnableUpgradeable,
   PausableUpgradeable
 {
-  mapping(uint32 => string[]) public providers;
+  // The instance of the EAS contract.
+  IEAS eas;
 
+  // Mapping of version to provider arrays
+  mapping(uint32 => string[]) public providerVersions;
+
+  // Version number
   uint32 public version;
 
+  // Address of the GitcoinResolver contract
   address public gitcoinResolverAddress;
+
+  // Passport credential struct
+  struct Credential {
+    string provider;
+    bytes32 hash;
+    uint64 issuanceDate;
+    uint64 expirationDate;
+  }
+
+  event PassportDecoded(Credential[] passport);
 
   function initialize(address _gitcoinResolverAddress) public initializer {
     __Ownable_init();
@@ -42,11 +61,19 @@ contract GitcoinPassportDecoder is
   function _authorizeUpgrade(address) internal override onlyOwner {}
 
   /**
-   * @dev Adds a new provider to the end of the providers mapping
+   * @dev Sets the address of the EAS contract.
+   * @param _easContractAddress The address of the EAS contract.
+   */
+  function setEASAddress(address _easContractAddress) public onlyOwner {
+    eas = IEAS(_easContractAddress);
+  }
+
+  /**
+   * @dev Adds a new provider to the end of the providerVersions mapping
    * @param provider Name of individual provider
    */
   function addProvider(string memory provider) external {
-    providers[version].push(provider);
+    providerVersions[version].push(provider);
   }
 
   /**
@@ -55,7 +82,13 @@ contract GitcoinPassportDecoder is
    */
   function createNewVersion(string[] memory providerNames) external {
     version++;
-    providers[version] = providerNames;
+    providerVersions[version] = providerNames;
+  }
+
+  function getAttestation(bytes32 attestationUID) public payable virtual returns (Attestation memory) {
+    console.log("getting attestation ");
+    Attestation memory attestation = eas.getAttestation(attestationUID);
+    return attestation;
   }
 
   /**
@@ -64,12 +97,81 @@ contract GitcoinPassportDecoder is
    * @param schemaUID The UID of the schema used to make the user's attestation
    */
   // getPassport(address): Calls GitcoinResolver to get the passport, then uses the provider mapping to decode the bits in the provider bitmap. This function can handle any bitmap version. The version is stored in the attestation, and the contract has all the data for historical bitmap versions. This should also demux the dates and hashes, so that the user gets back a normal looking passport object.
-  function getPassport(address userAddress, bytes32 schemaUID) public view returns (bytes32) {
+  function getPassport(address userAddress, bytes32 schemaUID) public returns (Credential[] memory) {
+    // Set the GitcoinResolver
     GitcoinResolver resolver = GitcoinResolver(gitcoinResolverAddress);
 
+    // Get the attestation UID from the user's attestations
     bytes32 attestationUID = resolver.userAttestations(userAddress, schemaUID);
 
-    // TODO: decode passport
 
+    // Get the attestation from the user's attestation UID
+    Attestation memory attestation = getAttestation(attestationUID);
+
+    // Set up the variables to assign the attestion data output to
+    uint256[] memory providers;
+    bytes32[] memory hashes;
+    uint64[] memory issuanceDates;
+    uint64[] memory expirationDates;
+    uint16 providerMapVersion;
+
+    // Decode the attestion output
+    (providers, hashes, issuanceDates, expirationDates, providerMapVersion) = abi.decode(
+      attestation.data,
+      (uint256[], bytes32[], uint64[], uint64[], uint16)
+    );
+
+    // Set up the variables to record the bit and the index of the credential hash
+    uint256 bit;
+    uint256 hashIndex = 0;
+
+    // Set the list of providers to the provider map version
+    string[] memory mappedProviders = providerVersions[providerMapVersion];
+
+    // Now we iterate over the providers array and check each bit that is set
+    // If a bit is set
+    // we set the hash, issuanceDate, expirationDate, and provider to a Credential struct
+    // then we push that struct to the passport storage array
+
+    // Set the passport array to be returned to equal the length of the passport saved to storage (for the duration of the function call)
+    Credential[] memory passportMemoryArray = new Credential[](hashes.length);
+
+    // Populate the passport memory array
+    for (uint256 i = 0; i < mappedProviders.length; i++) {
+      bit = 1;
+      uint256 provider = uint256(providers[i]);
+      for (uint256 j = 0; j < 256; ) {
+        console.log('hashes[hashIndex]', mappedProviders[i]);
+        // Check that the provider bit is set
+        if ((provider) & bit > 0) {
+          // The provider bit is set --> set the provider, hash, issuance date, and expiration date to the stuct
+          Credential memory credential;
+          credential.provider = mappedProviders[i];
+          if (hashIndex < hashes.length) {
+            credential.hash = hashes[hashIndex];
+          }
+          if (hashIndex < issuanceDates.length) {
+            credential.issuanceDate = issuanceDates[hashIndex];
+          }
+          if (hashIndex < expirationDates.length) {
+            credential.expirationDate = expirationDates[hashIndex];
+          }
+          passportMemoryArray[i] = credential;
+          hashIndex += 1;
+        }
+        bit <<= 1;
+        unchecked {
+          ++j;
+        }
+      }
+      unchecked {
+        i += 256;
+      }
+    }
+    
+    emit PassportDecoded(passportMemoryArray);
+
+    // Return the memory passport array
+    return passportMemoryArray;
   }
 }
--- test/GitcoinPassportDecoder.ts
+++ test/GitcoinPassportDecoder.ts
@@ -1,117 +1,234 @@
-import { expect, version } from "chai";
+import { expect } from "chai";
 import { ethers } from "hardhat";
 import {
+  SchemaEncoder,
   ZERO_BYTES32,
   NO_EXPIRATION,
+  EAS,
 } from "@ethereum-attestation-service/eas-sdk";
 import {
   GitcoinAttester,
   GitcoinResolver,
   GitcoinPassportDecoder,
 } from "../typechain-types";
-import { encodedData } from "./helpers/mockAttestations";
 import { SCHEMA_REGISTRY_ABI } from "./abi/SCHEMA_REGISTRY_ABI";
 import { schemaRegistryContractAddress } from "./GitcoinResolver";
+import {
+  passportTypes,
+  fee1,
+  EAS_CONTRACT_ADDRESS,
+} from "./GitcoinVerifier";
+import { BytesLike } from "ethers";
 
-describe("GitcoinPassportDecoder", async () => {
-  let owner: any,
-    iamAccount: any,
-    recipient: any,
-    nonOwnerOrVerifier: any,
-    mockEas: any,
-    gitcoinResolver: GitcoinResolver,
-    gitcoinAttester: GitcoinAttester,
-    gitcoinPassportDecoder: GitcoinPassportDecoder;
+type Provider = {
+  // TODO: add provider type
+}
 
-  beforeEach(async function () {
-    const [
-      ownerAccount,
-      otherAccount,
-      recipientAccount,
-      mockEasContractAccount,
-      nonOwnerOrVerifierAccount,
-    ] = await ethers.getSigners();
+type Hash = {
+  hash: BytesLike;
+}
 
-    owner = ownerAccount;
-    iamAccount = otherAccount;
-    recipient = recipientAccount;
-    mockEas = mockEasContractAccount;
-    nonOwnerOrVerifier = nonOwnerOrVerifierAccount;
+type IssuanceDate = {
+  issuanceDate: number;
+}
 
+type ExpirationDate = {
+  expirationDate: number;
+}
+
+type PassportAttestation = {
+  providers: Provider[];
+  hashes: Hash[];
+  issuanceDates: IssuanceDate[];
+  expirationDates: ExpirationDate[];
+}
+
+// const providers = ["Twitter", "Google", "Ens"];
+const providers = [BigInt("9444733296762888765505")];
+const issuanceDates = [1694628559, 1695047108, 1693498086];
+const expirationDates = [1702404559, 1702823108, 1701274086];
+const hashes = [
+  "0xf760285ed09eb7bb0da39df5abd0adb608d410b357ab6415644d2b49aa64e5f1", "0x29b3eb7b8ee47cb0a9d83e7888f05ea5f61e3437752602282e18129d2d8b4024", "0x84c6f60094c95180e54fac3e9a5cfde8ca430e598e987504474151a219ae0d13",
+];
+const providerMapVersion = 1;
+
+const easEncodeStamp = () => {
+  const schemaEncoder = new SchemaEncoder(
+    "uint256[] providers, bytes32[] hashes, uint64[] issuanceDates, uint64[] expirationDates, uint16 providerMapVersion"
+  );
+
+  const encodedData = schemaEncoder.encodeData([
+    { name: "providers", value: providers, type: "uint256[]" },
+    { name: "hashes", value: hashes, type: "bytes32[]" },
+    { name: "issuanceDates", value: issuanceDates, type: "uint64[]" },
+    { name: "expirationDates", value: expirationDates, type: "uint64[]" },
+    { name: "providerMapVersion", value: providerMapVersion, type: "uint16" },
+  ]);
+  return encodedData;
+}
+
+describe("GitcoinPassportDecoder", async function () {
+  this.beforeAll(async function () {
+    const [ownerAccount, iamAcct, recipientAccount] =
+      await ethers.getSigners();
+
+    this.owner = ownerAccount;
+    this.iamAccount = iamAcct;
+    this.recipient = recipientAccount;
+
+    // Deploy GitcoinAttester
     const GitcoinAttester = await ethers.getContractFactory(
       "GitcoinAttester",
-      owner
+      this.owner
     );
-    gitcoinAttester = await GitcoinAttester.deploy();
-    await gitcoinAttester.connect(owner).initialize();
-    const gitcoinAttesterAddress = await gitcoinAttester.getAddress();
+    this.gitcoinAttester = await GitcoinAttester.deploy();
+    await this.gitcoinAttester.connect(this.owner).initialize();
+    this.gitcoinAttesterAddress = await this.gitcoinAttester.getAddress();
 
-    // Initialize the sdk with the address of the EAS Schema contract address
-    await gitcoinAttester.setEASAddress(mockEas);
-
-    const GitcoinResolver = await ethers.getContractFactory(
-      "GitcoinResolver",
-      owner
+    // Deploy GitcoinVerifier
+    const GitcoinVerifier = await ethers.getContractFactory(
+      "GitcoinVerifier",
+      this.owner
     );
+    this.gitcoinVerifier = await GitcoinVerifier.deploy();
 
-    gitcoinResolver = await GitcoinResolver.deploy();
+    await this.gitcoinVerifier
+      .connect(this.owner)
+      .initialize(
+        await this.iamAccount.getAddress(),
+        await this.gitcoinAttester.getAddress()
+      );
 
-    await gitcoinResolver
-      .connect(owner)
-      .initialize(mockEas.address, gitcoinAttesterAddress);
+    this.eas = new EAS(EAS_CONTRACT_ADDRESS);
+    await this.gitcoinAttester.setEASAddress(EAS_CONTRACT_ADDRESS);
 
-    const gitcoinResolverAddress = await gitcoinResolver.getAddress();
+    const chainId = await ethers.provider
+      .getNetwork()
+      .then((n: { chainId: any }) => n.chainId);
+
+    this.domain = {
+      name: "GitcoinVerifier",
+      version: "1",
+      chainId,
+      verifyingContract: await this.gitcoinVerifier.getAddress(),
+    };
+
+    this.getNonce = async (address: string) => {
+      return await this.gitcoinVerifier.recipientNonces(address);
+    };
 
     this.uid = ethers.keccak256(ethers.toUtf8Bytes("test"));
 
+    // Deploy GitcoinResolver
+    const GitcoinResolver = await ethers.getContractFactory(
+      "GitcoinResolver",
+      this.owner
+    );
+    this.gitcoinResolver = await GitcoinResolver.deploy();
+    await this.gitcoinResolver
+      .connect(this.owner)
+      .initialize(
+        EAS_CONTRACT_ADDRESS,
+        await this.gitcoinAttester.getAddress()
+      );
+
+    // Register schema for resolver
     const schemaRegistry = new ethers.Contract(
-      schemaRegistryContractAddress,
+      ethers.getAddress(schemaRegistryContractAddress),
       SCHEMA_REGISTRY_ABI,
-      owner
+      this.owner
     );
 
-    const schema = "uint256 eventId, uint8 voteIndex";
-    const resolverAddress = gitcoinResolverAddress;
-    const revocable = true;
+    this.stampSchemaInput = "uint256[] providers, bytes32[] hashes, uint64[] issuanceDates, uint64[] expirationDates, uint16 providerMapVersion";
+    this.resolverAddress = await this.gitcoinResolver.getAddress();
+    this.revocable = true;
 
-    const transaction = await schemaRegistry.register(
-      schema,
-      resolverAddress,
-      revocable
+    this.stampTx = await schemaRegistry.register(
+      this.stampSchemaInput,
+      this.resolverAddress,
+      this.revocable
     );
 
-    await transaction.wait();
+    this.passportSchemaTxReceipt = await this.stampTx.wait();
+    const passportSchemaEvent = this.passportSchemaTxReceipt.logs.filter(
+      (log: any) => {
+        return log.fragment.name == "Registered";
+      }
+    );
+    this.passportSchemaUID = passportSchemaEvent[0].args[0];
+
+    this.passport = {
+      multiAttestationRequest: [
+        {
+          schema: this.passportSchemaUID,
+          data: [
+          //   {
+          //     recipient: this.recipient.address,
+          //     expirationTime: 1708741995,
+          //     revocable: true,
+          //     refUID: ZERO_BYTES32,
+          //     data: easEncodeStamp(),
+          //     value: 0,
+          //   },
+            {
+              recipient: this.recipient.address,
+              expirationTime: 1708741995,
+              revocable: true,
+              refUID: ZERO_BYTES32,
+              data: easEncodeStamp(),
+              value: 0,
+            },
+          ],
+        },
+      ],
+      nonce: await this.getNonce(this.recipient.address),
+      fee: fee1,
+    };
+
+    const addVerifierResult = await this.gitcoinAttester
+      .connect(this.owner)
+      .addVerifier(await this.gitcoinVerifier.getAddress());
+
+    await addVerifierResult.wait();
 
     const GitcoinPassportDecoder = await ethers.getContractFactory(
       "GitcoinPassportDecoder",
-      owner,
+      this.owner,
     );
 
-    gitcoinPassportDecoder = await GitcoinPassportDecoder.deploy();
+    this.gitcoinPassportDecoder = await GitcoinPassportDecoder.deploy();
 
-    await gitcoinPassportDecoder
-      .connect(owner)
-      .initialize(gitcoinResolverAddress);
+    await this.gitcoinPassportDecoder
+      .connect(this.owner)
+      .initialize(this.resolverAddress);
+
+    // Initialize the sdk with the address of the EAS Schema contract address
+    await this.gitcoinPassportDecoder.setEASAddress(EAS_CONTRACT_ADDRESS);
+  });
+  
+  this.beforeEach(async function () {
+    this.passport.nonce = await this.gitcoinVerifier.recipientNonces(
+      this.passport.multiAttestationRequest[0].data[0].recipient
+    );
   });
 
   describe("Creating new versions", function () {
-    it.only("should add new providers to the providers mapping and increment the version", async function () {
+    it("should add new providers to the providers mapping and increment the version", async function () {
       const providers = ["NewStamp1", "NewStamp2"];
-
       // Get the 0th version
-      const versionZero = await gitcoinPassportDecoder.version();
+      const versionZero = await this.gitcoinPassportDecoder.version();
 
       expect(versionZero === 0);
 
-      await gitcoinPassportDecoder.connect(owner).createNewVersion(providers);
+      await this.gitcoinPassportDecoder.connect(this.owner).createNewVersion(providers);
 
       // Get the current version
-      const currentVersion = await gitcoinPassportDecoder.version();
+      const currentVersion = await this.gitcoinPassportDecoder.version();
 
       expect(currentVersion === 1);
 
-      const firstProvider = await gitcoinPassportDecoder.providers(currentVersion, 0);
-
+      const firstProvider = await this.gitcoinPassportDecoder.providerVersions(currentVersion, 0);
+      
       expect(firstProvider === providers[0]);
     });
 
@@ -122,7 +239,41 @@ describe("GitcoinPassportDecoder", async () => {
 
   });
 
-  describe("", async function () {
+  describe("Decoding Passports", async function () {
+    it.only("should decode a user's passport", async function () {
+      const signature = await this.iamAccount.signTypedData(
+        this.domain,
+        passportTypes,
+        this.passport
+      );
 
+      const { v, r, s } = ethers.Signature.from(signature);
+
+      // Submit attestations
+      const verifiedPassport = await this.gitcoinVerifier.verifyAndAttest(
+        this.passport,
+        v,
+        r,
+        s,
+        {
+          value: fee1,
+        }
+      );
+
+      await verifiedPassport.wait();
+
+      const passportTx = await this.gitcoinPassportDecoder
+        .connect(this.owner)
+        .getPassport(this.recipient.address, this.passportSchemaUID);
+
+      const receipt = await passportTx.wait();
+
+      console.log(receipt.logs);
+      
+
+      // const passportEvents = await this.gitcoinPassportDecoder.queryFilter(this.gitcoinPassportDecoder.filters.PassportDecoded(), passportTx.blockNumber);
+      // console.log("Events -=-=-=-=>", passportEvents.queryFilter());
+      
+    });
   });
 });
