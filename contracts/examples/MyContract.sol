7bb549b78b9dc706abb296fadb2635f939870997
Gerald Iakobinyi-Pich <nutrina9@gmail.com>
2023-11-15 15:28:26 +0200

    feat: add comments to the example smart contract

--- contracts/examples/MyContract.sol
+++ contracts/examples/MyContract.sol
@@ -1,23 +1,29 @@
 // SPDX-License-Identifier: GPL
 pragma solidity ^0.8.9;
 
-import "../GitcoinPassportDecoder.sol";
+import "../IGitcoinPassportDecoder.sol";
 
 /**
- * @title GitcoinVerifier
- * @notice This contract is used to verify a passport's authenticity and to add a passport to the GitcoinAttester contract using the verifyAndAttest() function.
+ * @title MyContract
+ * @notice This is just a dummy smart contract that provides a permission method for incrementing a counter.
+ * Permissions are determined by humanity score
  */
 contract MyContract {
     /// Counter variable
     uint256 private counter;
 
     /// Threshold for humanity score
-    /// TODO: it would make sense to add threshold variable to decoder contract porbably
+    /// TODO: it would make sense to add threshold variable to the GitcoinPassportDecoder contract porbably
     uint256 private threshold;
 
-    GitcoinPassportDecoder _decoder;
+    IGitcoinPassportDecoder _decoder;
+
+    ////////////////////////////////////////////////////////
+    // Below are some example of modifiers (contraints/ permission checks)
+    ////////////////////////////////////////////////////////
 
     // This would be possible once we roll out the decoder smart contract
+    // This has the drawback, then the integrator needs to keep track of the threshold
     modifier onlyHuman() {
         require(threshold > 0, "Threshold not set");
         require(
@@ -28,16 +34,16 @@ contract MyContract {
     }
 
     // TODO: this is not scheduled yet
-    // This would be an even simpler version
+    // This would be an even simpler version compared to onlyHuman
     // It requires that the threshold (at least the default recommended one) is stored in the decoder
     modifier onlyHumanV2() {
         require(_decoder.isHuman(msg.sender), "Only humans allowed");
         _;
     }
 
-    // TODO: this is not scheduled yet
-    // This would be an even simpler version
-    // It requires that the threshold (at least the default recommended one) is stored in the decoder
+    // This would be possible once we roll out the decoder smart contract
+    // This is an example where an integrator gates a SC function based on the caller holding a specific 
+    // credential
     modifier onlyGitcoinDonors() {
         Credential[] memory credentials = _decoder.getPassport(msg.sender);
         bool hasGitcoinStamp = false;
@@ -67,12 +73,22 @@ contract MyContract {
         _;
     }
 
+    /**
+     * The integrator needs to set the address of the GitcoinPassportDecoder contract
+     * @param _decoderAddress the address of the GitcoinPassportDecoder contract
+     */
     function setGitcoinPassportDecoderAddress(
         address _decoderAddress
     ) external {
-        _decoder = GitcoinPassportDecoder(_decoderAddress);
+        _decoder = IGitcoinPassportDecoder(_decoderAddress);
     }
 
+    /**
+     * The integrator needs to set the threshold
+     * @param _threshold the threshold
+     * 
+     * @notice the threshold could also be maintaained in the GitcoinPassportDecoder
+     */
     function setThreshold(uint256 _threshold) external {
         threshold = _threshold;
     }
@@ -88,5 +104,3 @@ contract MyContract {
     }
 }
 
-
-https://gist.github.com/nutrina/a59727e1681b7b783ddaebed3beeb2f4
