c67a6d7f19bff9710bfbc58d58eb4e08c0743460
schultztimothy <schultz.timothy52@gmail.com>
2023-12-06 14:36:24 -0700

    single iteration test

--- test/IdStaker.ts
+++ test/IdStaker.ts
@@ -1,7 +1,7 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe.only("StakeTest Contract", function () {
+describe("StakeTest Contract", function () {
   let stakeTest;
   let owner;
   let addr1;
@@ -153,6 +153,164 @@ describe.only("StakeTest Contract", function () {
 
     it("Should add and sum 20 stakes using selfStakeIdsMap", async function () {
       await addStakes(owner, "addSelfStakeMap", 20);
+      await stakeTest.sumActiveSelfStakeMap(owner.address);
+      expect(await stakeTest.activeStake()).to.equal(ethers.parseEther("20"));
+    });
+  });
+});
+
+describe.only("StakeTest Contract", function () {
+  let stakeTest;
+  let owner;
+  let addr1;
+  let addr2;
+  let addrs;
+
+  beforeEach(async function () {
+    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+    const IdentityStakeGuardian = await ethers.getContractFactory(
+      "IdentityStakeGuardian"
+    );
+    stakeTest = await IdentityStakeGuardian.deploy();
+  });
+
+  describe("Adding and verifying values in communityStakeIds", function () {
+    it("Should add 5 values and verify one of them", async function () {
+      const valuesToAdd = [1];
+      await stakeTest.addValuesToCommunityStakeIds(
+        owner.address,
+        addr1.address,
+        valuesToAdd
+      );
+
+      // Verify one of the values
+      expect(await stakeTest.listLookup(owner.address, addr1.address, 3)).to.be
+        .true;
+      expect(await stakeTest.listLookup(owner.address, addr1.address, 6)).to.be
+        .false;
+    });
+  });
+
+  describe("Adding and verifying values in communityStakeIdsMap", function () {
+    it("Should add 5 values and verify one of them", async function () {
+      const valuesToAdd = [1];
+      await stakeTest.addValuesToCommunityStakeIdsMap(
+        owner.address,
+        addr1.address,
+        valuesToAdd
+      );
+
+      // Verify one of the values
+      expect(await stakeTest.listLookupMap(owner.address, addr1.address, 3)).to
+        .be.true;
+      expect(await stakeTest.listLookupMap(owner.address, addr1.address, 6)).to
+        .be.false;
+    });
+  });
+  describe("Adding and verifying values in communityStakeIdsMap", function () {
+    it("Should add 5 values and verify one of them", async function () {
+      const valuesToAdd = [1];
+      await stakeTest.addValuesToCommunityStakeIdsMap(
+        owner.address,
+        addr1.address,
+        valuesToAdd
+      );
+
+      // Verify one of the values
+      expect(await stakeTest.listLookupMap(owner.address, addr1.address, 3)).to
+        .be.true;
+      expect(await stakeTest.listLookupMap(owner.address, addr1.address, 6)).to
+        .be.false;
+    });
+  });
+  describe("20 iterations", function () {
+    describe("Adding and verifying values in communityStakeIds", function () {
+      it("Should add 20 values and verify one of them", async function () {
+        const valuesToAdd = Array.from({ length: 1 }, (_, i) => i + 1);
+        await stakeTest.addValuesToCommunityStakeIds(
+          owner.address,
+          addr1.address,
+          valuesToAdd
+        );
+
+        // Verify one of the values
+        expect(await stakeTest.listLookup(owner.address, addr1.address, 10)).to
+          .be.true;
+        expect(await stakeTest.listLookup(owner.address, addr1.address, 21)).to
+          .be.false;
+      });
+    });
+
+    describe("Adding and verifying values in communityStakeIdsMap", function () {
+      it("Should add 20 values and verify one of them", async function () {
+        const valuesToAdd = Array.from({ length: 1 }, (_, i) => i + 1);
+        await stakeTest.addValuesToCommunityStakeIdsMap(
+          owner.address,
+          addr1.address,
+          valuesToAdd
+        );
+
+        // Verify one of the values
+        expect(await stakeTest.listLookupMap(owner.address, addr1.address, 10))
+          .to.be.true;
+        expect(await stakeTest.listLookupMap(owner.address, addr1.address, 21))
+          .to.be.false;
+      });
+    });
+  });
+
+  async function addStakes(user, method, count) {
+    for (let i = 0; i < count; i++) {
+      const stake = { amount: ethers.parseEther("1"), unlockTime: 0 };
+      await stakeTest[method](user.address, stake);
+    }
+  }
+
+  describe("Adding stakes and summing active stakes", function () {
+    it("Should add and sum 5 stakes using selfStakeIds", async function () {
+      await addStakes(owner, "addSelfStake", 1);
+      await stakeTest.sumActiveSelfStake(owner.address);
+      expect(await stakeTest.activeStake()).to.equal(ethers.parseEther("5"));
+    });
+
+    it("should remove the last item from the array", async function () {
+      await addStakes(owner, "addSelfStake", 1);
+      await stakeTest.removeItemFromSelfIds(owner.address, 1);
+      expect(0).to.equal(ethers.parseEther("0"));
+    });
+
+    it("should remove the last item from the array", async function () {
+      await addStakes(owner, "addSelfStake", 5);
+      await stakeTest.removeItemFromSelfStakeIdsMap(owner.address, 1);
+      expect(0).to.equal(ethers.parseEther("0"));
+    });
+
+    it("should remove the last item from the array", async function () {
+      await addStakes(owner, "addSelfStake", 1);
+      await stakeTest.removeItemFromSelfIds(owner.address, 1);
+      expect(0).to.equal(ethers.parseEther("0"));
+    });
+
+    it("should remove the last item from the array", async function () {
+      await addStakes(owner, "addSelfStake", 1);
+      await stakeTest.removeItemFromSelfStakeIdsMap(owner.address, 1);
+      expect(0).to.equal(ethers.parseEther("0"));
+    });
+
+    it("Should add and sum 20 stakes using selfStakeIds", async function () {
+      await addStakes(owner, "addSelfStake", 1);
+      await stakeTest.sumActiveSelfStake(owner.address);
+      expect(await stakeTest.activeStake()).to.equal(ethers.parseEther("20"));
+    });
+
+    it("Should add and sum 5 stakes using selfStakeIdsMap", async function () {
+      await addStakes(owner, "addSelfStakeMap", 1);
+      await stakeTest.sumActiveSelfStakeMap(owner.address);
+      expect(await stakeTest.activeStake()).to.equal(ethers.parseEther("5"));
+    });
+
+    it("Should add and sum 20 stakes using selfStakeIdsMap", async function () {
+      await addStakes(owner, "addSelfStakeMap", 1);
       await stakeTest.sumActiveSelfStakeMap(owner.address);
       expect(await stakeTest.activeStake()).to.equal(ethers.parseEther("20"));
     });
