5bc65cd612b31ae3b78ef6df15457eabc7ba43d1
Gerald Iakobinyi-Pich1 <nutrina9@gmail.com>
2024-06-05 07:12:16 +0300

    feat: have updated deplyResolver.ts for zkSync, and have also deployed the resolver to skSync

--- scripts/deployResolver.ts
+++ scripts/deployResolver.ts
@@ -4,12 +4,12 @@ import hre, { ethers, upgrades } from "hardhat";
 import {
   assertEnvironment,
   confirmContinue,
-  updateDeploymentsFile,
-  getAbi,
   getAttesterAddress,
-  getEASAddress,
-  getScoreSchema
+  getEASAddress
 } from "./lib/utils";
+
+import { deployResolver } from "./lib/resolver";
+import { deployZkSyncResolver } from "./lib/zk-resolver";
 
 assertEnvironment();
 
@@ -22,31 +22,14 @@ export async function main() {
     network: hre.network.name,
     chainId: hre.network.config.chainId,
     attesterAddress: attesterAddress,
-    easAddress: easAddress,
+    easAddress: easAddress
   });
 
-
-  const GitcoinResolver = await ethers.getContractFactory("GitcoinResolver");
-  const resolver = await upgrades.deployProxy(
-    GitcoinResolver,
-    [easAddress, attesterAddress],
-    {
-      initializer: "initialize",
-      kind: "uups"
-    }
-  );
-
-  const deployment = await resolver.waitForDeployment();
-
-  const resolverAddress = await deployment.getAddress();
-
-  console.log(`✅ Deployed GitcoinResolver to ${resolverAddress}.`);
-
-  await updateDeploymentsFile(
-    "GitcoinResolver",
-    getAbi(deployment),
-    resolverAddress
-  );
+  if (hre.network.zksync) {
+    deployZkSyncResolver(attesterAddress, easAddress);
+  } else {
+    deployResolver(attesterAddress, easAddress);
+  }
 }
 
 main().catch((error) => {
