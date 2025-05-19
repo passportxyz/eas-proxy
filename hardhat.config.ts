398bdf33599d1be915f2a152215b6d3d5085c435
Lucian Hymer <lucian@gitcoin.co>
2025-05-19 07:46:18 -0600

    chore: remaining deployments

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -46,8 +46,10 @@ let config: HardhatUserConfig = {
   },
   etherscan: {
     apiKey: {
+      arbitrum: process.env.ARBISCAN_API_KEY as string,
       "eth-mainnet": process.env.ETHERSCAN_API_KEY as string,
       "optimism-sepolia": process.env.ETHERSCAN_API_KEY_OP as string,
+      optimisticEthereum: process.env.ETHERSCAN_API_KEY_OP as string,
       scroll: process.env.SCROLL_ETHERSCAN_API_KEY as string,
       "scroll-sepolia": process.env.SCROLL_SEPOLIA_ETHERSCAN_API_KEY as string,
       shape: "dummy api key",
